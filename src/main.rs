904e02dda4e3b49e89c120d6e1f38f76393f51ed
Joerg Zeidler <joerg.zeidler@conplement.de>
2021-11-12 09:15:40 +0100

    use unsafe wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
-use serde_json::{json, Value};
 use ics_dm_azure_rs::*;
-
-
-fn twin_callback(data: IotHubModuleEvent, state: IotHubModuleState) {
-
-    println!("twin_callback data: {:?} {:?}", data, state);
-
-    if let IotHubModuleEvent::Twin(new) = data {
-        println!("twin_callback new: {}", new);
-    }
-    let dummy: String = "{\"blubb\": \"supp\"}".to_string();
-    self.twin_report(&dummy);
-}
+use std::{thread, time};
 
 fn main() {
+    let result = iot_hub_init();
+    if result != 0 {
+        panic!("iot_hub_init not OK!");
+    }
 
-    let mut _client = IotHubModuleClient::new(twin_callback);
+    let handle;
+    let connection;
+    match get_connection_info_from_identity_service() {
+        Ok(date) => {
+            connection = date;
+            handle = create_from_connection_string(connection);
+        }
+        Err(e) => {
+            panic!("{}", e);
+        }
+    }
 
-    // let dummy: String = "{\"blubb\": \"rupp\"}".to_string();
-    // _client.twin_report(&dummy);
+    if handle.is_null() {
+        panic!("no valid handle received");
+    }
 
-    println!("Seems everthing is fine!!!");
-    _client.do_work();
+    match set_module_twin_callback(handle) {
+        Ok(()) => {
+            println!("set twin callback successfully");
+        }
+        Err(e) => {
+            panic!("{}", e);
+        }
+    }
+
+    loop {
+        do_work(handle);
+        let hundred_millis = time::Duration::from_millis(100);
+        thread::sleep(hundred_millis);
+    }
 }
