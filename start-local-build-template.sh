2ed7f2aa90a30050e6a7df67f52d7cf5bf738e8f
JanZachmann <jan.zachmann@conplement.de>
2022-02-09 08:18:35 +0100

    fixed text

--- start-local-build-template.sh
+++ start-local-build-template.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Hint: Both of the supported ICS_DeviceManagement targets
-# [yocto](https://github.com/ICS-DeviceManagement/ics-dm-os) and
+# [yocto](https://github.com/ICS-DeviceManagement/meta-ics-dm) and
 # [simulator](https://github.com/ICS-DeviceManagement/simulator)
-# integrate the ics-dm-iot-module-rs.
+# integrate iot-module-template-rs.
 
-# If you like to built it independently the paths from the libraries listed below are required.
+# If you intend to built the binary independently the paths from the libraries listed below are required.
 # Copy `.start-local-build-template.sh` to `.start-local-build.sh` and adapt it.
 # Example:
 # export LIB_PATH_AZURESDK=/home/user/projects/GitHub/simulator/build/.conan/data/azure-iot-sdk-c/LTS_01_2022_Ref01/_/_/package/3bf7811c9395d29095bf663023235996901b6af2
