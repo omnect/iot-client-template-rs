3ea6744ee27862f14b404e78792ed02dd6d46394
JanZachmann <jan.zachmann@conplement.de>
2022-02-24 12:55:30 +0100

    qa

--- src/message.rs
+++ src/message.rs
@@ -6,7 +6,7 @@ use azure_iot_sdk::message::*;
 
 pub fn update(msg: IotMessage, _tx_app2client: Arc<Mutex<Sender<Message>>>) {
     debug!(
-        "Received c2d message with \n body: {:?}\n properties: {:?} \n system properties: {:?}",
+        "Received C2D message with \n body: {:?}\n properties: {:?} \n system properties: {:?}",
         std::str::from_utf8(&msg.body).unwrap(),
         msg.properties,
         msg.system_properties
