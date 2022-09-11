0ab862e072b78c590c3a1c2048fdc7b8d22500ae
JanZachmann <jan.zachmann@conplement.de>
2022-09-11 14:00:15 +0200

    log error on panics

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use log::{error, info};
 use std::process;
 
 fn main() {
+    log_panics::init();
+
     if cfg!(debug_assertions) {
         Builder::from_env(Env::default().default_filter_or("debug")).init();
     } else {
@@ -11,7 +13,8 @@ fn main() {
     }
 
     info!("module version: {}", env!("CARGO_PKG_VERSION"));
-    info!("azure sdk version: {}", IotHubClient::get_sdk_version_string());
+    info!("azure sdk version: {}", IotHubClient::get_sdk_version_string()
+    );
 
     if let Err(e) = iot_client_template_rs::run() {
         error!("Application error: {}", e);
