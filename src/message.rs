9bc4ac192bf20a8fe052b8e2e19babd9d266cb49
Tobias Langer <tobias.langer@conplement.de>
2023-03-01 11:55:41 +0100

    remove locks on sender-side of mpsc channel
    Remove the locks on the shared app2client Sender. Senders implement the Send Trait. This allows them to be shared by copying and moving them to other threads (for example, see the "Shared Usage" example in
    https://doc.rust-lang.org/std/sync/mpsc).
    
    This simplifies the function calls and the unpacking logic. Furthermore, this reduces overall locking, as the Sender internally takes care of synchronization.

--- src/message.rs
+++ src/message.rs
@@ -2,9 +2,8 @@ use crate::Message;
 use azure_iot_sdk::client::IotMessage;
 use log::debug;
 use std::sync::mpsc::Sender;
-use std::sync::{Arc, Mutex};
 
-pub fn update(msg: IotMessage, _tx_app2client: Arc<Mutex<Sender<Message>>>) {
+pub fn update(msg: IotMessage, _tx_app2client: &Sender<Message>) {
     debug!(
         "Received C2D message with \n body: {:?}\n properties: {:?} \n system properties: {:?}",
         std::str::from_utf8(&msg.body).unwrap(),
